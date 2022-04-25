7348312083a621c3c7c8d8b4191167e897b9a645
higi <cdramirezh@unal.edu.co>
2022-04-24 22:48:48 -0500

    Now /wiki/ is the root. Successfull test converting html to md

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponse #Delete this later when no longer needed
+from markdown2 import Markdown
 
 from . import util
-
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
+def test(request):
+    markdowner = Markdown()
+    route = '/home/higi/dev/edx/wiki/entries/HTML.md'
+    with open(route, 'r') as file:
+        data = file.read()
+    test = markdowner.convert(data)
+    return HttpResponse(test)
